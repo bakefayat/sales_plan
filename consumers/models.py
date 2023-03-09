8df1b9e882f4076fe83c3045cc65fda5a82322f2
bakefayat <bakefayat98@gmail.com>
2023-03-09 13:15:08 +0330

    consumers model modified.

--- consumers/models.py
+++ consumers/models.py
@@ -4,6 +4,13 @@ from django.db import models
 
 
 class Consumers(models.Model):
-    name = models.CharField(max_length=150)
-    family = models.CharField(max_length=150)
-    number_of_members = models.IntegerField()
+    class Meta:
+        verbose_name = 'مصرف کننده'
+        verbose_name_plural = 'مصرف کنندگان'
+
+    name = models.CharField(max_length=150, verbose_name='نام')
+    family = models.CharField(max_length=150, verbose_name='نام خانوادگی')
+    number_of_members = models.IntegerField(verbose_name='تعداد اعضای خانواه')
+
+    def __str__(self):
+        return f'{self.name} {self.family}'
