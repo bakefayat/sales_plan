13be73a45d3047e4b96071a89593ef8d4b4362f0
bakefayat <bakefayat98@gmail.com>
2023-03-09 14:11:12 +0330

    SalesPlan model added.

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from consumers.models import Consumers
+from core.models import TimeStampedModel
 
-# Create your models here.
+
+class SalesPlan(TimeStampedModel):
+    consumer = models.ManyToManyField(Consumers, related_name='plans', verbose_name='مصرف کننده')
