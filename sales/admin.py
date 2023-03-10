58fa4086a3d816a49477f65140645dd2b2650666
bakefayat <bakefayat98@gmail.com>
2023-03-10 13:39:01 +0330

    SalesPlan and Sellers models added.

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from sales.models import Sellers, SalesPlan
 
-# Register your models here.
+admin.site.register(Sellers)
+admin.site.register(SalesPlan)
--- sales/models.py
+++ sales/models.py
@@ -3,5 +3,26 @@ from consumers.models import Consumers
 from core.models import TimeStampedModel
 
 
+class Sellers(models.Model):
+    class Meta:
+        verbose_name = 'فروشنده'
+        verbose_name_plural = 'فروشندگان'
+
+    store_name = models.CharField(max_length=255, verbose_name='نام واحد تجاری')
+    address = models.TextField(verbose_name='آدرس')
+    phone_number = models.CharField(max_length=12, verbose_name='تلفن تماس')
+
+    def __str__(self):
+        return self.store_name
+
+
 class SalesPlan(TimeStampedModel):
+    class Meta:
+        verbose_name = 'طرح فروش'
+        verbose_name_plural = 'طرح های فروش'
+
     consumer = models.ManyToManyField(Consumers, related_name='plans', verbose_name='مصرف کننده')
+    seller = models.ManyToManyField(Sellers, related_name='seller', verbose_name='فروشنده')
+
+    # def __str__(self):
+    #     return self.seller
