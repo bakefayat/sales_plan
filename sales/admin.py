092763e3f0404b7e01e712522abfe1fba97e75e0
bakefayat <bakefayat98@gmail.com>
2023-03-10 17:54:04 +0330

    SalesPlan and Sellers models modified.

--- sales/admin.py
+++ sales/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from sales.models import Sellers, SalesPlan
+from sales.models import Sellers, SalesPlan, Sells
+
 
 admin.site.register(Sellers)
 admin.site.register(SalesPlan)
+admin.site.register(Sells)
--- sales/models.py
+++ sales/models.py
@@ -3,6 +3,20 @@ from consumers.models import Consumers
 from core.models import TimeStampedModel
 
 
+class SalesPlan(TimeStampedModel):
+    class Meta:
+        verbose_name = 'طرح فروش'
+        verbose_name_plural = 'طرح های فروش'
+
+    title = models.CharField(max_length=255, verbose_name='نام طرح')
+    is_active = models.BooleanField(default=True, verbose_name='فعال بودن')
+    total_capacity = models.IntegerField(verbose_name='حداکثر ظرفیت طرح')
+    total_sales = models.IntegerField(default=0, verbose_name='تعداد شرکت کنندگان')
+
+    def __str__(self):
+        return self.title
+
+
 class Sellers(models.Model):
     class Meta:
         verbose_name = 'فروشنده'
@@ -16,13 +30,14 @@ class Sellers(models.Model):
         return self.store_name
 
 
-class SalesPlan(TimeStampedModel):
+class Sells(models.Model):
     class Meta:
-        verbose_name = 'طرح فروش'
-        verbose_name_plural = 'طرح های فروش'
+        verbose_name = 'فروش'
+        verbose_name_plural = 'فروش ها'
 
-    consumer = models.ManyToManyField(Consumers, related_name='plans', verbose_name='مصرف کننده')
-    seller = models.ManyToManyField(Sellers, related_name='seller', verbose_name='فروشنده')
+    consumer = models.ForeignKey(Consumers, related_name='sell', unique=True, verbose_name='مصرف کننده', on_delete=models.CASCADE)
+    seller = models.ForeignKey(Sellers, related_name='sell', null=True, verbose_name='فروشنده', on_delete=models.SET_NULL)
+    sale_plan = models.ForeignKey(SalesPlan, related_name='sell', verbose_name='طرح فروش', on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     return self.seller
+    def __str__(self):
+        return f'{self.consumer} از {self.seller}'
