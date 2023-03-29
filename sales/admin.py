7235903e49732d8b4b762868b9480072ba65ab38
bakefayat <bakefayat98@gmail.com>
2023-03-29 08:48:10 +0330

    address now added to sellersAdmin page.

--- sales/admin.py
+++ sales/admin.py
@@ -7,11 +7,11 @@ class SalesPlanAdmin(admin.ModelAdmin):
 
 
 class SellersAdmin(admin.ModelAdmin):
-    list_display = ('user', 'store_name')
+    list_display = ('user', 'store_name', 'address')
 
 
 class SellsAdmin(admin.ModelAdmin):
-    list_display = ('consumer', 'sale_plan', 'seller', 'created', 'is_valid')
+    list_display = ('consumer', 'sale_plan', 'seller', 'created')
 
 
 admin.site.register(Sellers, SellersAdmin)
