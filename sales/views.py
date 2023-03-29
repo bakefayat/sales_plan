43f1c88930494318a9df90e7447fbf66e6a35de7
bakefayat <bakefayat98@gmail.com>
2023-03-29 08:57:27 +0330

    not valid form added to project. TODO: Use templates to show that.

--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
+from django.db import IntegrityError
+from django.http import HttpResponse
 
 from accounts.mixins import SellerSuperuserMixin, FieldsMixin, FormValidMixin
 from .models import SalesPlan, Sells
@@ -20,3 +22,9 @@ class SellCreate(LoginRequiredMixin, SellerSuperuserMixin, FormValidMixin, Field
     template_name = 'sales/sell_register.html'
     model = Sells
     success_url = reverse_lazy('sales:plans_list')
+
+    def form_valid(self, form):
+        try:
+            return super().form_valid(form)
+        except IntegrityError:
+            return HttpResponse('not valid!')
